41d63dc8d53dc2f48b40788945d9abb7b6a6fbaf
DevenNan <devenamazingnandapurkar@gmail.com>
2022-12-08 10:24:06 +0530

    pages and routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function about() {
+  return (
+    <div>
+      <h1> Hellow Worlkd</h1>
+      <h3> this is an app to find the latets DJ gigs happening in you town! </h3>
+      <h3>
+        Stay tuned for more to come events! 
+      </h3>
+    </div>
+  )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function add() {
+  return (
+    <div>
+      <h1>Add event</h1>
+    </div>
+  )
+}
